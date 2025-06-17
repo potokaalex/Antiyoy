13d617dc660cbb4d1995cad1755ce371e4801cf6
PotokaAlex <nevskial686@gmail.com>
2025-06-17 17:53:19 +0300

    ConfigsController

---
+++ Antiyoy/Assets/Client/Code/Services/Config/ConfigData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Client.Code.Services.Config
+{
+    [CreateAssetMenu(menuName = "Client/Configs/Main", fileName = "ConfigData", order = 0)]
+    public class ConfigData : ScriptableObject
+    {
+
+    }
+}
---
+++ Antiyoy/Assets/Client/Code/Services/Config/ConfigsController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Client.Code.Services.Config
+{
+    public class ConfigsController : IInitializable, IProvider<ConfigData>
+    {
+        private ConfigData _configData;
+
+        ConfigData IProvider<ConfigData>.Value => _configData;
+        
+        public void Initialize() => _configData = Resources.Load<ConfigData>("ConfigData");
+    }
+}
