a6aa881ddcfdc2b52c5bec4510b3c919cc1315c3
PotokaAlex <nevskial686@gmail.com>
2026-07-22 23:54:47 +0300

    Experiment view borders view

--- Assets/Client/Tile/TilemapController.cs
+++ Assets/Client/Tile/TilemapController.cs
@@ -8,6 +8,7 @@ namespace Client.Tile
   public class TilemapController : MonoBehaviour, IInitializable
   {
     [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private Tilemap _tilemapShadows;
     [SerializeField] private TileBase _tile;
     private GridController _gridController;
 
@@ -17,17 +18,26 @@ namespace Client.Tile
       FillByBaseTiles();
     }
 
-    public void SetColor(HexCoordinates position, Color color) =>
-      _tilemap.SetColor(_gridController.GridIndexFrom2DIndex(position.ToArray2DIndex()), color);
+    public void SetColor(HexCoordinates position, Color color)
+    {
+      var tilemapPosition = _gridController.GridIndexFrom2DIndex(position.ToArray2DIndex());
+      _tilemap.SetColor(tilemapPosition, color);
+      _tilemapShadows.SetColor(tilemapPosition, Color.white);
+    }
 
     private void FillByBaseTiles()
     {
       var size = _gridController.Size;
       for (var y = 0; y < size.y; y++)
       for (var x = 0; x < size.x; x++)
-        _tilemap.SetTile(_gridController.GridIndexFrom2DIndex(new Vector2Int(x, y)), _tile);
+      {
+        var tilemapPosition = _gridController.GridIndexFrom2DIndex(new Vector2Int(x, y));
+        _tilemap.SetTile(tilemapPosition, _tile);
+        _tilemapShadows.SetTile(tilemapPosition, _tile);
+      }
 
       _tilemap.CompressBounds();
+      _tilemapShadows.CompressBounds();
     }
   }
 }
