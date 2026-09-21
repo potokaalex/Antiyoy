dd0b76ee665f83112701f72a4b06943089f90338
PotokaAlex <nevskial686@gmail.com>
2026-09-22 00:12:13 +0300

    Protection shields animations

---
+++ Assets/Client/Protection/ProtectionIconData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Client.Protection
+{
+  public struct ProtectionIconData
+  {
+    public SpriteRenderer Renderer;
+    public Transform Transform;
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+  }
+}
